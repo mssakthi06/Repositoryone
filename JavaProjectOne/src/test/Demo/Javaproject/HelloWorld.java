1a9824c4df8e8ad344e14d6f94d82833821d2a00
c018683 <c018683@WN-OHA00SA4L897.us.bank-dns.com>
2019-08-13 15:33:39 -0400

    Added Encapsualtion and Abstraction files per Req# 3.4.2.3.
    Change-Id: I56a01d0854ca63243723057be4107f5828d49912
    Signed-off-by: c018683 <c018683@WN-OHA00SA4L897.us.bank-dns.com>

---
+++ JavaProjectOne/src/test/Demo/Javaproject/HelloWorld.java
@@ -0,0 +1,10 @@
+package test.Demo.Javaproject;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Kungumai kaliamman Thunai");
+	}
+
+}
---
+++ JavaProjectOne/src/test/Demo/Javaproject/Polymorphism.java
@@ -0,0 +1,9 @@
+package test.Demo.Javaproject;
+
+public class Polymorphism {
+	
+	public static void main(String[] args) {
+	System.out.println("polymorphism");
+	System.out.println("Trying to implement few more");
+}
+}
